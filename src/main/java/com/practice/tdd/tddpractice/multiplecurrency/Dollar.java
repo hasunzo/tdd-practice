17dee27b17d0a2dcac08d64423496cadaf6e2110
hasunzo <gktjsdl470@gmail.com>
2021-10-22 22:33:09 +0900

    update Dollar

--- src/main/java/com/practice/tdd/tddpractice/multiplecurrency/Dollar.java
+++ src/main/java/com/practice/tdd/tddpractice/multiplecurrency/Dollar.java
@@ -4,9 +4,10 @@ public class Dollar {
     int amount;
 
     public Dollar(int amount) {
+        this.amount = amount;
     }
 
     void times(int multiplier) {
-
+        this.amount = multiplier * amount;
     }
 }
